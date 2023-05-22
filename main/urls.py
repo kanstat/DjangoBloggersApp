be21557c593a17fe11954967a3675b7b17648052
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-22 13:34:04 +0530

    new uel forget_password is added

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('loginpg/',views.login,name='login'),
-    path('signuppg/',views.signup,name="signup")
+    path('', views.home, name='home'),
+    path('loginpg/', views.login, name='login'),
+    path('signuppg/', views.signup, name="signup"),
+    path('forgot_password/', views.forget_passcode, name='forget_passcode'),
 ]
