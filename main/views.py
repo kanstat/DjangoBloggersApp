376f99c48c30795250b38e35597d14d8aa3c3cdd
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-22 13:35:17 +0530

    view function of forget password is created(initail phase)

--- main/views.py
+++ main/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from . forms import UserForm
 from . models import User
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,"base.html",{"hii":"hello"})
+    return render(request, "base.html", {"hii": "hello"})
 
 
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        query = User.objects.filter(username=username,password=password)
+        query = User.objects.filter(username=username, password=password)
         if query.exists():
-            return render(request,"login.html",{"message":"login sucessful"})
+            return render(request, "login.html", {"message": "login sucessful"})
 
-    return render(request,"login.html",{"message":"Enter valid username and password"})
-        
-        
+    return render(request, "login.html", {"message": "Enter valid username and password"})
 
 
 def signup(request):
@@ -27,12 +27,20 @@ def signup(request):
         email = request.POST["email"]
         # form = UserForm(request.POST)
         # if form.is_valid():
-            # extracting user data from form 
-            # username = form.cleaned_data['username']
-            # password = form.cleaned_data['password']
-            # email = form.cleaned_data['email']
-            # # creating user
-        user = User(username=username,password=password,email=email)
-            # save user into the database
+        # extracting user data from form
+        # username = form.cleaned_data['username']
+        # password = form.cleaned_data['password']
+        # email = form.cleaned_data['email']
+        # # creating user
+        user = User(username=username, password=password, email=email)
+        # save user into the database
         user.save()
-    return render(request,"signup.html")
+    return render(request, "signup.html")
+
+
+def forget_passcode(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        if User.objects.filter(email=email).exists():
+            pass
+    return render(request, "forgetpasscode.html")
