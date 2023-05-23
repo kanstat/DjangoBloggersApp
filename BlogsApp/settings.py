86f522743dcd68fe6cc7669a683ddc7a5c0cb523
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-23 10:27:38 +0530

    mail configrations added in settings.py

--- BlogsApp/settings.py
+++ BlogsApp/settings.py
@@ -136,8 +136,12 @@ STATICFILES_DIRS = (
 )
 
 
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'woofunds.io@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
