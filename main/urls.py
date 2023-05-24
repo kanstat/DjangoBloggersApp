d7351457e6bd0f80ae6d287f82c9f93726e95d05
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-24 13:25:42 +0530

    two new urls dashboard and login_logic is added

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('loginpg/', views.login, name='login'),
+    path('login_logic', views.login_logic, name="login_logic"),
     path('signuppg/', views.signup, name="signup"),
     path('signup_logic/', views.signup_logic, name='signup_logic'),
     path('forgot_password/', views.forget_passcode, name='forget_passcode'),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
