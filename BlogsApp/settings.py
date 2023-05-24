d810f345f147249690205dfa21a16958b048eee0
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-24 19:04:44 +0530

    updated .gitignore

--- BlogsApp/settings.py
+++ BlogsApp/settings.py
@@ -86,6 +86,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'djangodb',
+#         'USER': 'kanstat',
+#         'PASSWORD': 'xsvk0101',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
