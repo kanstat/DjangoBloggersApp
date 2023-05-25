6c7c14519944fec8a0cd86c4af9bd50fb3998563
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-25 17:36:40 +0530

    two new urls are added for email verification and forget_pass validation

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('loginpg/', views.login, name='login'),
     path('login_logic', views.login_logic, name="login_logic"),
+    path('activate/<user_id>/<token>/', views.activate, name="activate"),
     path('signuppg/', views.signup, name="signup"),
     path('signup_logic/', views.signup_logic, name='signup_logic'),
     path('forgot_password/', views.forget_passcode, name='forget_passcode'),
+    path('forgot_password_validation/', views.forget_password_validation,
+         name="forgot_pass_validation"),
     path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from . forms import UserForm
 from . models import User
 from django.contrib.auth.hashers import make_password, check_password
+from django.utils.encoding import smart_str
 from django.utils.encoding import force_bytes
 # urlsafe_base64_encode() and urlsafe_base64_decode() functions in Django are used to encode and decode data in a way that is safe
 # to use in URLs. The urlsafe_base64_encode() function takes a string as input and returns a base64-encoded string that does not
@@ -27,6 +28,16 @@ def login(request):
     return render(request, 'login.html')
 
 
+def activate(request, user_id, token):
+    # uid = request.GET.get('user_id')
+    uid = smart_str(urlsafe_base64_decode(user_id))
+    user = User.objects.get(id=uid)
+    if user:
+        user.email_verification = 1
+        user.save()
+    return render(request, "login.html")
+
+
 def login_logic(request):
     if request.method == "POST":
         email = request.POST['email']
@@ -36,7 +47,8 @@ def login_logic(request):
             hashed_pasword = user_obj.password
             if user_obj:
                 res = check_password(raw_password, hashed_pasword)
-                if res:
+
+                if res and user_obj.email_verification == 1:
                     session_id = uuid_genrator()
                     user_obj.session_id = session_id
                     user_obj.save()
@@ -45,7 +57,12 @@ def login_logic(request):
                     response.set_cookie('session_id', session_id)
                     return response
         except:
-            return render(request, "login.html", {"message": "incorrect email or password"})
+            return render(request, "login.html", {"message": "incorrect email or password or verify ypur email first"})
+
+
+def getcookies(request):
+    value = request.COOKIES.get('session_id')
+    return value
 
 
 def signup(request):
@@ -72,23 +89,28 @@ def signup_logic(request):
             user_id = urlsafe_base64_encode(force_bytes(user.id))
             # token generator
             token = uuid_genrator()
-            link = f"http://127.0.0.1:8000/activate/{user_id}/{token}"
+            link = f"http://127.0.0.1:8000/activate/{user_id}/{token}/"
             subject = "Registration"
             email_content = f"Hi {user.username},\n\nThank you for signing up for our service. To verify your email address, please click on the following link:\n\n{link}\n\nIf you do not click on the link within 24 hours, your account will be deleted.\n\nThanks,\n[Django Bloggers]"
             email_from = settings.EMAIL_HOST_USER
             recepient_list = [email,]
             send_mail(subject, email_content, email_from, recepient_list)
+            user = User(email_verification=1)
     return render(request, "login.html", {"message": "User register sucessfully..."})
 
 
 def forget_passcode(request):
     if request.method == "POST":
         email = request.POST["email"]
-        if User.objects.filter(email=email).exists():
-            # user_obj =
+        user = User.objects.get(email=email)
+        if user.exists():
             pass
+
     return render(request, "forgetpasscode.html")
 
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    val = getcookies(request)
+    user = User.objects.get(session_id=val)
+    if user:
+        return render(request, "dashboard.html")
