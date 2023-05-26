043d098d75a1cbeba01fcd6a6493b98d06922b4c
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-26 13:42:16 +0530

    two new urls are added

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('signuppg/', views.signup, name="signup"),
     path('signup_logic/', views.signup_logic, name='signup_logic'),
     path('forgot_password/', views.forget_passcode, name='forget_passcode'),
-    path('forgot_password_validation/', views.forget_password_validation,
+    path('forgot_password_logic/', views.forgot_password_logic,
+         name="forgot_password_logic"),
+    path('reset/<user_id>/<token>', views.forgot_password_validation,
          name="forgot_pass_validation"),
     path('dashboard/', views.dashboard, name="dashboard"),
 ]
