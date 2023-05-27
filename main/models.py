05983eb41e38a0723d50c9751b8a5de1c21eecc1
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-27 14:01:52 +0530

    max_length changed

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=30)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     email = models.EmailField()
     session_id = models.CharField(max_length=100, default=0)
     time_stamp = models.DateTimeField(default=timezone.now)
