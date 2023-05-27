82d0c966deac73b22666b567421573f44f0d9cd1
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-05-27 14:23:41 +0530

    mail changed

--- BlogsApp/settings.py
+++ BlogsApp/settings.py
@@ -155,7 +155,7 @@ MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'woofunds.io@gmail.com'
+EMAIL_HOST_USER = 'teddysingh540@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
