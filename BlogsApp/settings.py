11bf312127ed446131a7858258172c0429f615f8
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-06-22 14:46:54 +0530

    rest_framework is addded in seetings.py

--- BlogsApp/settings.py
+++ BlogsApp/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # app
     'main',
+    # other apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
